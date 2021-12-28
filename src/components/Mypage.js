78ec31ec9ee283c4f40d50055ed96036cfb87e75
padd60 <padd60@naver.com>
2021-12-28 09:55:48 +0900

    My Page Init

---
+++ src/components/Mypage.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { SiCashapp } from "react-icons/si";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+const Mypage = () => {
+  let navigate = useNavigate();
+
+  // styled component
+  let TopTitle = styled("p")`
+    font-size: 48px;
+  `;
+
+  let Line = styled("div")`
+    width: 80%;
+    height: 5px;
+    background-color: #2d4059;
+    margin: 0 auto;
+  `;
+  // end styled component
+  return (
+    <div>
+      <div className="cotainer-lg">
+        <div
+          className="row d-flex justify-content-center"
+          style={{ width: "100%", paddingTop: "50px" }}
+        >
+          <div
+            className="col-lg-1"
+            onClick={() => {
+              navigate("/");
+            }}
+            style={{ marginBottom: "20px", cursor: "pointer" }}
+          >
+            <SiCashapp
+              style={{ color: "#EA5455", fontSize: "70px", margin: "0 20px" }}
+            />
+          </div>
+          <div className="col-lg-9">
+            <TopTitle>Check items & Feed back</TopTitle>
+          </div>
+        </div>
+      </div>
+      <Line></Line>
+    </div>
+  );
+};
+
+export default Mypage;
